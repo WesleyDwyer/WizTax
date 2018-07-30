dce7353ecebe5331d2e48cf32576dad5c499bc15
Wesley Dwyer <wesley.dwyer@solutechinnovation.com>
2018-07-30 13:51:09 -0500

    Add project files.

---
+++ WizTax/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WizTax.Startup))]
+namespace WizTax
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
